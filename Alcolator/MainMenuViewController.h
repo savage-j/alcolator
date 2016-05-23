cd7c94d440d0bd6263a6d3350cce16d1b1793d8e
Jordan <savage@savagej.com>
2016-05-22 19:43:02 -0700

    Add navigation view controller for wine and whiskey

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Jordan Carlson on 5/22/16.
+//  Copyright © 2016 savagej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
