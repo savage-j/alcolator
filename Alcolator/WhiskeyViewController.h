19a3c451ae1e9b96929ca7c2e02e982edc6f86b3
Jordan <savage@savagej.com>
2016-05-22 19:10:55 -0700

    Connect storyboards to code

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Jordan Carlson on 5/22/16.
+//  Copyright © 2016 savagej. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
